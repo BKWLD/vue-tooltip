a9e18d3ec788695e30ad704b597d68fa893f0f6a
Jonathan Jahr <jonjahr@gmail.com>
2022-05-17 13:43:39 -0700

    First commit

---
+++ index.vue
@@ -0,0 +1,27 @@
+<template lang='pug'>
+
+.tooltip I'm a tooltip
+
+  //- Slotted in content
+  slot
+
+</template>
+
+<!-- ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<script lang='coffee'>
+
+export default
+
+	name: 'VueTooltip'
+
+
+</script>
+
+<!-- ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<style lang='stylus'>
+@import '~bukwild-stylus-library/index.styl'
+
+
+</style>
